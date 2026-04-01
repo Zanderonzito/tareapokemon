d313f723e13695de8f7aa1728c4c30059501baeb
ZanderVT <124890404+Zanderonzito@users.noreply.github.com>
2026-04-01 19:28:39 -0300

    Refactor data cleaning function and variable names

--- Tarea Pokemon.py
+++ Tarea Pokemon.py
@@ -1,16 +1,15 @@
 import pandas as pd
 #1. Lectura de datos
-#-------------------
 #- Carga el archivo pokemon_primera_gen.csv en un DataFrame de Pandas.
-def clean_data(df):
-    df = df.drop_duplicates()
-    df = df.fillna({'Nombre': "No posee"})
-    df = df.fillna({'Tipo 2': "No posee"})
-    return df
-df = pd.read_csv(r'c:\Users\X-40845\Desktop\pokemon_primera_gen.csv')
+def clean_data(pokemones):
+    pokemones = pokemones.drop_duplicates()
+    pokemones = pokemones.fillna({'Nombre': "No posee"})
+    pokemones = pokemones.fillna({'Tipo 2': "No posee"})
+    return pokemones
+pokemones = pd.read_csv(r'c:\Users\X-40845\Desktop\pokemon_primera_gen.csv')
 
-df_clean = clean_data(df.copy())
-df_clean.head()
+pokemones_clean = clean_data(pokemones.copy())
+pokemones_clean.head()
 
 #2. Filtrado y selección
 #-----------------------
