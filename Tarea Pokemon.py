fd3764eb040612ee73ffff2b00f913b01dd73f41
icyfavvi <kiddonachou@gmail.com>
2026-04-01 23:01:02 -0300

    Update Tarea Pokemon.py

--- Tarea Pokemon.py
+++ Tarea Pokemon.py
@@ -1,18 +1,23 @@
 import pandas as pd
+
+# !!! FAVOR PODER FILLEAR MISSING VALUES EN DATA WRANGLER EN SUS PC´S, A MI NO ME DEJA HACERLO :(
+
 #1. Lectura de datos
 #- Carga el archivo pokemon_primera_gen.csv en un DataFrame de Pandas.
-def clean_data(pokemones):
-    pokemones = pokemones.drop_duplicates()
-    pokemones = pokemones.fillna({'Nombre': "No posee"})
-    pokemones = pokemones.fillna({'Tipo 2': "No posee"})
-    return pokemones
-pokemones = pd.read_csv(r'c:\Users\X-40845\Desktop\pokemon_primera_gen.csv')
-pokemones_clean = clean_data(pokemones.copy())
-pokemones_clean.head()
-
+file = "pokemon_primera_gen_datos_limpios.csv"
+df = pd.read_csv(file)
+df.head()
 #2. Filtrado y selección
 #-----------------------
 #- Filtra todos los Pokémon de tipo "Fuego".
 #- Selecciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
+fuego = df[df["Tipo 1"] == "Fuego"]
+print("=== Pokémon de tipo Fuego ===")
+print(fuego)
+
+columnas = df[["Nombre", "Tipo 1", "Ataque", "Velocidad"]]
+print("\n=== Columnas seleccionadas ===")
+print(columnas.head())
+
 
 
