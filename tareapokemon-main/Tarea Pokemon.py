248bf6f732154c34ee9892d060cb140d3d612152
Martin Caamaño <mcaamano@utem.cl>
2026-04-02 20:35:44 -0300

    Tarea 2 Completa ahora sin errores

--- tareapokemon-main/Tarea Pokemon.py
+++ tareapokemon-main/Tarea Pokemon.py
@@ -5,19 +5,32 @@ import pandas as pd
 #1. Lectura de datos
 #- Carga el archivo pokemon_primera_gen.csv en un DataFrame de Pandas.
 file = "pokemon_primera_gen_datos_limpios.csv"
-df = pd.read_csv(file)
-df.head()
+pk = pd.read_csv(file)
+pk.head()
+
+
 #2. Filtrado y selección
 #-----------------------
 #- Filtra todos los Pokémon de tipo "Fuego".
 #- Selecciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
-fuego = df[df["Tipo 1"] == "Fuego"]
-print("=== Pokémon de tipo Fuego ===")
-print(fuego)
+#- puse el Tipo 2 como adicional por que algunos pokemons tienen segundo tipo
 
-columnas = df[["Nombre", "Tipo 1", "Ataque", "Velocidad"]]
-print("\n=== Columnas seleccionadas ===")
-print(columnas.head())
+def filtrar_pokemon_fuego(pk):
+    Fuego = pk[pk["Tipo 1"] == "Fuego"]
+    filtrado = Fuego[["Nombre","Tipo 1","Tipo 2","Ataque","Velocidad"]]
+    return filtrado
+
+#Mostrar datos para luego agregar a un menu
+pk_fuego = filtrar_pokemon_fuego(pk)
+print(pk_fuego)
 
 
+#3. Estadística descriptiva básica
+#---------------------------------
+#- Calcula el promedio, la mediana y la moda del ataque de todos los Pokémon.
+#- ¿Cuál es el Pokémon con mayor defensa? ¿Y el de menor velocidad?
+#- ¿Cuántos Pokémon tienen dos tipos?
+#- Calcula el rango y la desviación estándar de los PS (Puntos de Salud).
+
+#no tengo idea de como hacer un comit
 
