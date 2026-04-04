476be9394de88a530926d63b5016e6519b0a64ef
Martin Caamaño <mcaamano@utem.cl>
2026-04-03 21:41:10 -0300

    Datos Limpios 100% y Ejercicio 3 Listo

--- tareapokemon-main/Tarea Pokemon.py
+++ tareapokemon-main/Tarea Pokemon.py
@@ -28,9 +28,37 @@ print(pk_fuego)
 #3. Estadística descriptiva básica
 #---------------------------------
 #- Calcula el promedio, la mediana y la moda del ataque de todos los Pokémon.
+def Estadisticas(pk):
+    promedio = pk["Ataque"].mean()
+    mediana = pk["Ataque"].median()
+    moda = pk["Ataque"].mode()[0]
+
+    print("Promedio: ",round(promedio))
+    print("Mediana: ",mediana)
+    print("Moda: ",moda)
+
 #- ¿Cuál es el Pokémon con mayor defensa? ¿Y el de menor velocidad?
+def MayoryMenor(pk):
+    Mayordefensa = pk.loc[pk["Defensa"].idxmax()]
+    Menorvelocidad = pk.loc[pk["Velocidad"].idxmin()]
+    print("El pokemon com mas defensa es: ",Mayordefensa["Nombre"],"con un total de:",Mayordefensa["Defensa"])
+    print("El pokemon con menor velocidad es:",Menorvelocidad["Nombre"],"con un total de:",Menorvelocidad["Velocidad"])
+
 #- ¿Cuántos Pokémon tienen dos tipos?
+def Dostipos(pk):
+    Numero = pk[pk["Tipo 2"] != "No posee"].shape[0]
+    print("Hay",Numero,"pokemons con 2 tipos")
+
 #- Calcula el rango y la desviación estándar de los PS (Puntos de Salud).
+def Saludpokemon(pk):
+    Rango = pk["PS"].max() - pk["PS"].min()
+    Desviacion = pk["PS"].std()
+    print("El rango Puntos de salud de los pokemons es de:",Rango)
+    print("La desviacion estandar de de los Puntos de salud es de:",round(Desviacion))
 
-#no tengo idea de como hacer un comit
-
+#muestra los datos para luego agregar a un menu
+print (Estadisticas(pk))
+print (MayoryMenor(pk))
+print(Dostipos(pk))
+print(Saludpokemon(pk))
+#El que haga el menu, que tambien haga un segundo menu para este ejercicio (3)
