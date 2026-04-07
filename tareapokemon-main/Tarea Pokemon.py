72f3d750e9097f7eb1e1bd342423c7bee5790dce
Martin Andrés Caamaño Jofre <martincj120@gmail.com>
2026-04-06 20:48:11 -0400

    Arreglando errores

--- tareapokemon-main/Tarea Pokemon.py
+++ tareapokemon-main/Tarea Pokemon.py
@@ -8,7 +8,7 @@ import seaborn as sns            #APRETAR CTRL + P , escribir PYTHON y seleccion
 #- Carga el archivo pokemon_primera_gen.csv en un DataFrame de Pandas.
 def cargar_datos():
     try: 
-        file = "pokemon_primera_gen.csv"
+        file = "pokemon_primera_gen_datos_limpios.csv"
         pk = pd.read_csv(file)
         if pk.empty:
             print("Error: El archivo csv está vacío.")
@@ -20,13 +20,15 @@ def cargar_datos():
         print("Error: El archivo csv no fue encontrado. Revisa el nombre o la ruta.")
         return None
     except pd.errors.ParserError as e: #esto lo encontré en stackoverflow para poder evitar errores al leer el csv, es solo para asegurarnos, en el datawrangler vi cuestiones como repetidas xd
-        print(f"Error de análisis en el archivo CSV: {e}")
+        print("Error de análisis en el archivo CSV: (e)")
         return None
 #2. Filtrado y selección
 #-----------------------
 #- Filtra todos los Pokémon de tipo "Fuego".
 #- Selecciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
 #- puse el Tipo 2 como adicional por que algunos pokemons tienen segundo tipo
+
+pk = cargar_datos()
 
 def filtrar_pokemon_fuego(pk):
     Fuego = pk[pk["Tipo 1"] == "Fuego"]
